d0c4b38e62bafac2eb800fb7d3fe22276dae9f02
Víctor M. Reyes <victor.reyes@focusservices.com>
2023-03-14 21:05:02 -0600

    chore: add server with express generator

---
+++ 1-node/1.9-express/app.js
@@ -0,0 +1,41 @@
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+app.use("/health", (req, res, next) =>
+  res.status(200).send({ message: "Server OK" })
+);
+
+app.use("*", (req, res, next) =>
+  res.status(404).send({ message: "Not found" })
+);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
