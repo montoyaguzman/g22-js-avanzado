c657fe215b07616b7479ca640a27d7bfb815bb55
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-03-16 20:45:30 -0600

    feat: ejemplo de getById

--- 5-apis-examples/5.0-apis-g22/version-sin-scaffold/src/index.js
+++ 5-apis-examples/5.0-apis-g22/version-sin-scaffold/src/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3000; // process.ENV.PORT || 3000;
 
 const pokemons = [
-    { name: 'bulbasaur', type: 'grass' },
-    { name: 'squirtle', type: 'water' },
-    { name: 'charmander', type: 'fire' },
+    { id: 1, name: 'bulbasaur', type: 'grass' },
+    { id: 2, name: 'squirtle', type: 'water' },
+    { id: 3, name: 'charmander', type: 'fire' },
 ];
 
 app.get('/', (req, res) => {
@@ -16,8 +16,15 @@ app.get('/list-pokemon', (req, res) => {
     res.status(200).json(pokemons);
 });
 
-app.get('/list-pokemon/:id', (req, res) => {
-    
+app.get('/list-pokemon/:id/', (req, res) => {
+    const id = req.params.id;
+    console.log('req.params: ', id);
+    console.log('req.params: ', typeof id);
+    // aqui iria la consulta la bd con el id
+    const foundedPokemon = pokemons.filter(pokemon => pokemon.id === parseInt(id) )[0];
+    console.log('foundedPokemon: ', foundedPokemon);
+    res.status(200).json(foundedPokemon);
+
 });
 
 app.listen(port, () => {
