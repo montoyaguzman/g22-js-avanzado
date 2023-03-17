52a5b76f8e52f92078dddf8db3da3b5e7244ad9a
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-03-16 21:40:01 -0600

    feat: agrega put a la api pokemon

--- 5-apis-examples/5.0-apis-g22/version-sin-scaffold/src/index.js
+++ 5-apis-examples/5.0-apis-g22/version-sin-scaffold/src/index.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000; // process.ENV.PORT || 3000;
 
-const pokemons = [
+app.use(express.json());
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+let pokemons = [
     { id: 1, name: 'bulbasaur', type: 'grass' },
     { id: 2, name: 'squirtle', type: 'water' },
     { id: 3, name: 'charmander', type: 'fire' },
@@ -24,9 +29,23 @@ app.get('/list-pokemon/:id/', (req, res) => {
     const foundedPokemon = pokemons.filter(pokemon => pokemon.id === parseInt(id) )[0];
     console.log('foundedPokemon: ', foundedPokemon);
     res.status(200).json(foundedPokemon);
-
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.post('/create-pokemon', (req, res) => {
+    const newPokemon = req.body;
+    console.log('newPokemon: ', newPokemon);
+    pokemons.push(newPokemon);
+    console.log('pokemons', pokemons);
+    res.status(201).send();
 });
+
+app.put('/edit-pokemon/:id', (req, res) => {
+    const id = req.params.id;
+    const newPokemon = req.body;
+    console.log('newPokemon: ', newPokemon);
+    const index = pokemons.findIndex(pokemon => pokemon.id === parseInt(id));
+    pokemons[index] = newPokemon;
+    console.log('pokemons', pokemons);
+    res.status(200).send();
+});
+
